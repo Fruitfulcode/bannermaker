b19765c9ddac269ab7b0ebaf877dbac65972fdb6
Yuriy <yurchenko@fruitfulcode.com>
2013-02-07 11:24:44 +0200

    delete asd

--- js/validate.js
+++ js/validate.js
@@ -1,34 +1,32 @@
-jQuery('#create_submit,#update_submit').live('click',function() {
-	/*var field = new Array("login", "pass", "confirmpass", "e-mail","about");//поля обязательные
-		   
-	$("form").submit(function() {// обрабатываем отправку формы   
-		var error=0; // индекс ошибки
-		$("form").find(":input").each(function() {// проверяем каждое поле в форме
-			for(var i=0;i<field.length;i++){ // если поле присутствует в списке обязательных
-				if($(this).attr("name")==field[i]){ //проверяем поле формы на пустоту
-				   
-					if(!$(this).val()){// если в поле пустое
-						$(this).css('border', 'red 1px solid');// устанавливаем рамку красного цвета
-						error=1;// определяем индекс ошибки      
-												   
+jQuery('#create_submit').live('click',function() {
+	var text_field = new Array("set_name"); /* add text field(id) here */
+    var nummeric_field = new Array("set_width", "set_height"); /* add numeric  field(id) here */
+	
+		var error=0; 
+		$("#banner_form_set").find(":input").each(function() {
+			/* Check text field */
+			for(var i=0;i<text_field.length;i++){ 
+				if($(this).attr("id") == text_field[i]){ 
+					if(!$(this).val()){
+						$(this).css('border', 'red 1px solid');
+						error=1;
 					}
 					else{
-						$(this).css('border', 'gray 1px solid');// устанавливаем рамку обычного цвета
+						$(this).css('border', 'gray 1px solid');
 					}
-				   
+				}              
+			}
+			/* Check numeric field */
+			for(var i=0;i<nummeric_field.length;i++){ 
+				if($(this).attr("id") == nummeric_field[i]){ 
+					if(!$(this).val()){
+						$(this).css('border', 'red 1px solid');
+						error=2;
+					}
+					else{
+						$(this).css('border', 'gray 1px solid');
+					}
 				}              
 			}
 	   })
-	   
-		if(error==0){ // если ошибок нет то отправляем данные
-			return true;
-		}
-		else{
-		if(error==1) var err_text = "Не все обязательные поля заполнены!";
-		$("#messenger").html(err_text);
-		$("#messenger").fadeIn("slow");
-		return false; //если в форме встретились ошибки , не  позволяем отослать данные на сервер.
-		}
-	})*/
-	alert('asd');
 });
