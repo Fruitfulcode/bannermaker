76fb1869986da91d8bafdc11d3245fffc66266d8
Yuriy <yurchenko@fruitfulcode.com>
2013-02-11 18:23:49 +0200

    11.02.2013

--- inc/file.php
+++ inc/file.php
@@ -1,8 +1,33 @@
-<?php
+<?php	
+global $xml_maccive;
+
 add_action('wp_ajax_banner_open_custom_css', 'banner_open_custom_css');
 
 function banner_open_custom_css() {
-    	
+	
+	
+/*	$dom = new DomDocument();
+	$dom->loadxml(BANNER_INC."/custom.xml");
+	$friends = $dom->getElementsByTagName('class');
+	print_r ($dom);
+	$dom->save(BANNER_INC."/custom.xml");  
+	*/
+	
+	
+/*	$doc = new DOMDocument();
+	$doc->load(BANNER_INC."/custom.xml");
+	$value = $doc->saveXML();
+	print_r ($doc);
+	echo $doc->textContent;*/
+	
+	$xml = simplexml_load_file(BANNER_INC."/custom.xml");
+	
+	
+		foreach ($xml as $val) {
+			echo 'name=  '.$val->name;
+			echo 'value= '.$val->value;
+		}
+		
 	die();
 }
 
